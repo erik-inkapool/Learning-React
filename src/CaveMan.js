1c893eea7facd0eb01d767270f93f52e4e39aa32
Erik Lundgren <erik@lundgren.cc>
2016-09-24 20:10:56 +0200

    Removed create-react-app code

---
+++ src/CaveMan.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CaveMan.css';
+
+class CaveMan extends Component {
+  render() {
+    return (
+      <div className="CaveMan">
+      Hello world
+      </div>
+    );
+  }
+}
+
+export default CaveMan;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CaveMan from './CaveMan';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <CaveMan />,
   document.getElementById('root')
 );
